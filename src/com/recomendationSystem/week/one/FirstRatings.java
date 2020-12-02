0db800ca862f7f28bcb50c5f8248916a5fa315f0
GeorgeLykoudis <george.lykoudis@yahoo.com>
2020-12-02 23:05:20 +0200

    create functions to search on rated movie file based on criteria

--- src/com/recomendationSystem/week/one/FirstRatings.java
+++ src/com/recomendationSystem/week/one/FirstRatings.java
@@ -9,7 +9,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author George Lykoudis
@@ -17,7 +19,8 @@ import java.util.List;
  */
 public class FirstRatings
 {
-    List<Movie> movies;
+    List<Movie>          movies;
+    Map<String, Integer> moviesPerDirector;
 
     public FirstRatings()
     {
@@ -79,11 +82,130 @@ public class FirstRatings
         movies = loadMovies(fileName);
         System.out.println("There are " + movies.size() + " in file " + fileName);
         printMovies();
+        System.out.println("There are " + countByGenre("Comedy") + " movies that belong to genre Comedy.");
+        System.out.println("There are " + countMoviesLongerThanMinutes(150) + " movies longer than 150 minutes.");
+
+        moviesPerDirector = countMoviesPerDirector();
+        for(Map.Entry<String,Integer> entry : moviesPerDirector.entrySet()) {
+            System.out.println("Director " + entry.getKey() + " has " + entry.getValue() + " movies");
+        }
+        System.out.println(moviesPerDirector.toString());
+        System.out.println("Maximum movies directed : " + maxNumberOfMoviesDirected());
+        System.out.println(directorsWithMaximumNumberOfMoviesDirected().toString());
     }
 
     private void printMovies() {
         for(Movie movie : movies) {
             System.out.println(movie.toString());
         }
+    }
+
+    /**
+     * Determine how many movies include the "genre" genre.
+     *
+     * @param genre
+     * @return
+     */
+    public int countByGenre(String genre)
+    {
+        int count = 0;
+        for(Movie movie : movies)
+        {
+            String[] genres = movie.getGenres().split(",");
+            for(String g : genres)
+            {
+                if(g.trim().equals(genre))
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Determine how many movies are greater than
+     * "minutes" minutes in length.
+     *
+     * @param minutes
+     * @return
+     */
+    public int countMoviesLongerThanMinutes(int minutes)
+    {
+        int count = 0;
+        for(Movie movie : movies)
+        {
+            if(movie.getMinutes() > minutes) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Determine the maximum number of movies by any director,
+     * and who the directors are that directed that many movies.
+     *
+     * @return
+     */
+    public Map<String,Integer> countMoviesPerDirector()
+    {
+        Map<String, Integer> moviesPerDirector = new HashMap<>();
+        for(Movie movie : movies)
+        {
+            String[] directors = movie.getDirector().split(",");
+            for(String d : directors)
+            {
+                d = d.trim();
+                if(!moviesPerDirector.containsKey(d)) {
+                    moviesPerDirector.put(d, 1);
+                }
+                else {
+                    moviesPerDirector.put(d, moviesPerDirector.get(d) + 1);
+                }
+            }
+        }
+        return moviesPerDirector;
+    }
+
+    /**
+     * Determine the maximum number of movies by any director.
+     *
+     * @return
+     */
+    private int maxNumberOfMoviesDirected()
+    {
+        int max = -1;
+        for(Integer value : moviesPerDirector.values())
+        {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Determine the maximum number of movies by any director,
+     * and who the directors are that directed that many movies.
+     *
+     * @return
+     */
+    public List<String> directorsWithMaximumNumberOfMoviesDirected()
+    {
+        int maxMovies = maxNumberOfMoviesDirected();
+        if(maxMovies < 1) {
+            return new ArrayList<>();
+        }
+        List<String> directors = new ArrayList<>();
+        for(Map.Entry<String,Integer> entry : moviesPerDirector.entrySet())
+        {
+            if(entry.getValue() == maxMovies) {
+                directors.add(entry.getKey());
+            }
+        }
+        return directors;
     }
 }
